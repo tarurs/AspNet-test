daf205c688a5e2ae4eccbb13f1db9ae53369490c
Sergey Kornev <tarurs67@outlook.com>
2018-02-24 13:59:17 +0200

    Full project from part 1

---
+++ WebApplication1/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
